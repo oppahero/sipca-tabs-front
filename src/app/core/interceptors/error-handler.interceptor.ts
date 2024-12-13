c494c3777ce7d93a665756a4ddd669f1c857281e
María López <sirlzn@sidor.com>
2024-12-13 12:38:45 -0400

    updated: Interceptors

--- src/app/core/interceptors/error-handler.interceptor.ts
+++ src/app/core/interceptors/error-handler.interceptor.ts
@@ -1,32 +1,35 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http'
-// import { inject } from '@angular/core'
+import { inject } from '@angular/core'
 import { catchError, throwError } from 'rxjs'
-// import { SessionExpiredService } from '../services/session-expired.service'
+import { SessionExpiredService } from '../services/session-expired.service'
 
 export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
-
-  // const sessionService= inject(SessionExpiredService)
+  const sessionService = inject(SessionExpiredService)
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-
       // Emitir que el token expiro
-      // if(error.status === 401) {
-      //   sessionService.emit(true)
-      // }
-
-      let responseError = error.status === 500 ? error.error.error : error.error
-
-      if(error.status === 0)
-        responseError= { message: 'No se ha podido establecer conexión con el servidor.'}
-
-      const err =  {
-        ...responseError,
-        status: error.status,
-        statusText: error.statusText
+      if (error.status === 401) {
+        sessionService.emit(true)
       }
 
-      return throwError(() => err )
+      let responseError =
+        error.status === 500 || error.status === 403 || error.status === 401
+          ? error.error.error
+          : error.error
+
+      if (error.status === 0)
+        responseError = {
+          message: 'No se ha podido establecer conexión con el servidor.',
+        }
+
+      const err = {
+        ...responseError,
+        status: error.status,
+        statusText: error.statusText,
+      }
+
+      return throwError(() => err)
     })
   )
 }
