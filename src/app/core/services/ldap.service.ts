591d9e035243c60eb82864a1014058a77247493a
María López <sirlzn@sidor.com>
2024-12-13 12:37:02 -0400

    updated: LdapService

--- src/app/core/services/ldap.service.ts
+++ src/app/core/services/ldap.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { Response } from '../models'
 import { environment } from 'src/environments/environment'
+import { LoginResponse } from '@core/models'
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +11,11 @@ export class LdapService {
   private _url: string
 
   constructor(private _http: HttpClient) {
-    this._url = environment.apiUrlLDAP + 'ldap/login'
+    this._url = environment.apiUrl + 'auth/ldap'
   }
 
-  login(data: any): Observable<Response> {
-    return this._http.post<Response>(this._url, data)
+  login(data: any): Observable<LoginResponse> {
+    return this._http.post<LoginResponse>(this._url, data)
   }
 
 }
