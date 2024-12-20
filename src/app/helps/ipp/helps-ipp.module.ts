be0046078238eeb7ecf616546fa8b0dc4872f71f
María López <sirlzn@sidor.com>
2024-12-20 13:44:27 -0400

    add: helps ipp module

---
+++ src/app/helps/ipp/helps-ipp.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { SharedModule } from '../../shared/shared.module'
+// import { ImpresoraModule } from '../APT/impresora/impresora.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharedModule,
+    // ImpresoraModule
+  ],
+  exports: [],
+
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class HelpsIppModule {}
