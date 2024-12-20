fccb82fa080ecdd5e0ac97ddee1e3e1dcbd9da32
María López <sirlzn@sidor.com>
2024-12-20 13:45:05 -0400

    add: helps ofa module

---
+++ src/app/helps/ofa/index.ts
@@ -0,0 +1,7 @@
+export * from './ofa/ofa.component';
+export * from './ofaOv/ofaOv.component';
+export * from './ofa-pp/ofa-pp.component';
+export * from './ofa-lote/ofa-lote.component';
+export * from './contadores-linea/contadores-linea.component';
+export * from './programado-linea/programado-linea.component';
+export * from './programado-avance/programado-avance.component';
